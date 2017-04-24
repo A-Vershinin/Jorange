6037cb6f9c2ae4377f364e8591fb43cb9be6aff8
Anatoliy Vershinin <cata07@yandex.ru>
2017-04-25 01:04:17 +0300

    add fonts, header

--- gulp/paths/css.bundle.js
+++ gulp/paths/css.bundle.js
@@ -2,5 +2,6 @@
 
 module.exports = [
   './bower_modules/normalize-css/normalize.css',
+  './bower_modules/font-awesome/css/font-awesome.css',
   './bower_modules/css-hamburgers/dist/hamburgers.css',
 ];
