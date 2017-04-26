b6e66623e8e351738b8e88a99e926a5ee53c419d
Anatoliy Vershinin <cata07@yandex.ru>
2017-04-26 21:41:45 +0300

    add work

--- gulp/paths/js.bundle.js
+++ gulp/paths/js.bundle.js
@@ -3,5 +3,6 @@
 module.exports = [
   './bower_modules/picturefill/dist/picturefill.js',
   './bower_modules/jquery/dist/jquery.js',
-  './bower_modules/bootstrap/dist/js/bootstrap.js'
+  './bower_modules/bootstrap/dist/js/bootstrap.js',
+  './bower_modules/owl.carousel/dist/owl.carousel.js'
 ];
